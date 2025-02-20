ef586566d4c91e1faccc78b87e37fa8f54a184da
Kaioken200x <luiz_henrique_933@hotmail.com>
2025-02-20 14:33:54 +0100

    rutas

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\PaymentController;
->>>>>>> c854fd0 (prueba laravel)
 
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
-=======
 
 Route::post('/process-easymoney', [PaymentController::class, 'processEasyMoney']);
 Route::post('/pay-superwalletz', [PaymentController::class, 'paySuperWalletz']);
 Route::post('/superwalletz-callback', [PaymentController::class, 'superWalletzCallback']);
 Route::post('/deposit', [PaymentController::class, 'deposit']);
->>>>>>> c854fd0 (prueba laravel)
